4a73153c13a647d7325c30fdd6aa3b24d161e8f4
Huiqun Li <xun7yy@gmail.com>
2019-06-09 09:43:56 -0700

    inti

---
+++ js/data.js
@@ -0,0 +1,54 @@
+var data = {
+	categoryOptions: [
+	    {
+	    	id: 1,
+	    	categoryName: 'toy',
+	    	isChecked: true,
+	    	subcategoryList: ['ball', 'bell']
+	    },
+	    {
+	    	id: 2,
+	    	categoryName: 'health',
+	    	isChecked: true,
+	    	subcategoryList: ['pain', 'eye']
+	    }
+    ],
+	items: [
+		{
+			id: 1,
+			name: 'dog1',
+			price: 1,
+			pic: 'media/sample.jpg',
+			type: 'dog',
+			category: 'toy',
+			subcategory: 'balls'
+		},
+		{
+			id: 2,
+			name: 'dog2',
+			price: 2,
+			pic: 'media/sample.jpg',
+			type: 'dog',
+			category: 'health',
+			subcategory: 'pain'
+		},
+		{
+			id: 3,
+			name: 'cat1',
+			price: 3,
+			pic: 'media/sample.jpg',
+			type: 'cat',
+			category: 'toy',
+			subcategory: 'ball'
+		},
+		{
+			id: 4,
+			name: 'cat2',
+			price: 4,
+			pic:'media/sample.jpg',
+			type:'cat',
+			category: 'health',
+			subcategory: 'pain'
+		}
+	]
+};
